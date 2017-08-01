3bdcb129e554ac65c6c33a7aa2c2404900f714be
write <write@10.0.0.57>
2017-07-31 21:34:18 -0400

    Uodate Reports to reportsNG

---
+++ shalmi2/src/main/java/all_operations/Listeners.java
@@ -0,0 +1,74 @@
+package all_operations;
+
+import org.testng.IClass;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+public class Listeners implements ITestListener {
+	
+
+
+	@Override
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult tr) {
+		log("Test '" + tr.getName() + "' PASSED");
+
+		// This will print the class name in which the method is present
+		log(tr.getTestClass());
+
+		// This will print the priority of the method.
+		// If the priority is not defined it will print the default priority as
+		// 'o'
+		log("Priority of this method is " + tr.getMethod().getPriority());
+
+		System.out.println(".....");
+
+		
+	}
+	@Override
+	public void onTestFailure(ITestResult tr) {
+
+		log("Test '" + tr.getName() + "' FAILED");
+		log("Priority of this method is " + tr.getMethod().getPriority());
+		System.out.println(".....");
+	}
+	private void log(String methodName) {
+		System.out.println(methodName);
+	}
+
+	private void log(IClass testClass) {
+		System.out.println(testClass);
+	}
+
+}
